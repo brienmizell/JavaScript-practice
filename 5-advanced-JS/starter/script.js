ac9c86c5f08d9a4ed418ad40e0f53fb6666639c5
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-08-05 17:44:36 -0400

    Completed second part of coding challange.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -316,36 +316,99 @@
 /////////////////////////////
 // CODING CHALLENGE
 
-/*
---- Let's build a fun quiz game in the console! ---
+// --- Let's build a fun quiz game in the console! ---
 
-1. Build a function constructor called Question to describe a question. A question should include:
-a) question itself
-b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
-c) correct answer (I would use a number for this)
+// 1. Build a function constructor called Question to describe a question. A question should include:
+// a) question itself
+// b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+// c) correct answer (I would use a number for this)
 
-2. Create a couple of questions using the constructor
+// 2. Create a couple of questions using the constructor
 
-3. Store them all inside an array
+// 3. Store them all inside an array
 
-4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+// 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
 
-5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+// 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
 
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+// 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 
-7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+// 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 
---- Expert level ---
+// //////////////////////////////////////////////////////////////////////////////////////////////
 
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+// (function() {
+//   function Question(question, answers, correct) {
+//     this.question = question;
+//     this.answers = answers;
+//     this.correct = correct;
+//   }
 
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+//   Question.prototype.displayQuestion = function() {
+//     console.log(this.question);
 
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+//     for (var i = 0; i < this.answers.length; i++) {
+//       console.log(i + ": " + this.answers[i]);
+//     }
+//   };
 
-11. Display the score in the console. Use yet another method for this.
-*/
+//   Question.prototype.checkAnswer = function(ans) {
+//     if (ans === this.correct) {
+//       console.log("Correct answer!");
+//     } else {
+//       console.log("Wrong answer, try again.");
+//     }
+//   };
+
+//   var q1 = new Question(
+//     "Is JavaScrip the coolest programming language in the universe?",
+//     ["Yes", "No", "Maybe"],
+//     0
+//   );
+
+//   var q2 = new Question(
+//     "What MLS club invented soccer and is the best MLS club ever?",
+//     [
+//       "Orlando Shitty SC",
+//       "New Jersey Energy Drinks",
+//       "D.C. United",
+//       "Atlanta United"
+//     ],
+//     3
+//   );
+
+//   var q3 = new Question(
+//     "Which is the greatest college football team of all time?",
+//     [
+//       "University of Alabamuuuuh",
+//       "University of Georgia",
+//       "Auburn Kitty Cats",
+//       "North Avenue Trade School wasps"
+//     ],
+//     1
+//   );
+
+//   var questions = [q1, q2, q3];
+
+//   var n = Math.floor(Math.random() * questions.length);
+
+//   questions[n].displayQuestion();
+
+//   var answer = parseInt(prompt("Please Select the correct answer."));
+//   questions[n].checkAnswer(answer);
+// })();
+
+// //////////////////////////////////////////////////////////////////////////////////////////////
+
+// --- Expert level ---
+
+// 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+// 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+// 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+// 11. Display the score in the console. Use yet another method for this.
 
 (function() {
   function Question(question, answers, correct) {
@@ -362,12 +425,22 @@ c) correct answer (I would use a number for this)
     }
   };
 
-  Question.prototype.checkAnswer = function(ans) {
+  Question.prototype.checkAnswer = function(ans, callback) {
+    var sc;
     if (ans === this.correct) {
       console.log("Correct answer!");
+      sc = callback(true);
     } else {
       console.log("Wrong answer, try again.");
+
+      sc = callback(false);
     }
+    this.scoreDisplay(sc);
+  };
+
+  Question.prototype.scoreDisplay = function(score) {
+    console.log("Your score is: " + score);
+    console.log("----------------------------------");
   };
 
   var q1 = new Question(
@@ -400,10 +473,28 @@ c) correct answer (I would use a number for this)
 
   var questions = [q1, q2, q3];
 
-  var n = Math.floor(Math.random() * questions.length);
+  function score() {
+    var sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+  var scoreKeeper = score();
 
-  questions[n].displayQuestion();
+  function nextQuestion() {
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
+    var answer = prompt(
+      "Please Select the correct answer. \nType 'exit' to quit the game."
+    );
 
-  var answer = parseInt(prompt("Please Select the correct answer."));
-  questions[n].checkAnswer(answer);
+    if (answer !== "exit") {
+      questions[n].checkAnswer(parseInt(answer), scoreKeeper);
+      nextQuestion();
+    }
+  }
+  nextQuestion();
 })();

2eea0cac57ebcb080635e7880531991c32c227b9
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-08-08 18:40:02 -0400

    Able to add new items to budget controller.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,6 +1,64 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  // some code
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    },
+    budget: 0,
+    percentage: -1
+  };
+  console.log(data.allItems);
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+      //  [1, 2, 3, 4, 5], next ID = 6
+      //  [1, 2, 4, 6, 8], next ID = 9
+      // ID = last ID + 1
+      console.log(data.allItems[type]);
+      // Create new ID
+      if (budgetController.data.allItems[type].length > 0) {
+        ID =
+          budgetController.data.allItems[type][
+            budgetController.data.allItems[type].length - 1
+          ].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp' type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      // Push it into the data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 // UI CONTROLLER
@@ -28,7 +86,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
   var eventListenerSetup = function() {
-    var DOM = UIController.getDOMstrings();
+    var DOM = UICtrl.getDOMstrings();
 
     document
       .querySelector(DOM.inputButton)
@@ -42,9 +100,11 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. Get the input data
-    var input = UIController.getInput();
+    input = UICtrl.getInput();
     // 2. Add the item to budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI

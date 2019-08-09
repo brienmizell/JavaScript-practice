584a20a75a35e9355f7632dc16612181c28f1508
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-08-09 00:50:08 -0400

    Debugging.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -4,6 +4,7 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
 
   var Income = function(id, description, value) {
@@ -24,32 +25,31 @@ var budgetController = (function() {
     budget: 0,
     percentage: -1
   };
-  console.log(data.allItems);
+  console.log(data.allItems.type);
 
   return {
     addItem: function(type, des, val) {
+      console.log(this.addItem.type);
       var newItem, ID;
       //  [1, 2, 3, 4, 5], next ID = 6
       //  [1, 2, 4, 6, 8], next ID = 9
       // ID = last ID + 1
       console.log(data.allItems[type]);
       // Create new ID
-      if (budgetController.data.allItems[type].length > 0) {
-        ID =
-          budgetController.data.allItems[type][
-            budgetController.data.allItems[type].length - 1
-          ].id + 1;
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
       } else {
         ID = 0;
       }
 
       // Create new item based on 'inc' or 'exp' type
-      if (type === "exp") {
+      if (type === "expense") {
         newItem = new Expense(ID, des, val);
-      } else if (type === "inc") {
+      } else if (type === "income") {
         newItem = new Income(ID, des, val);
       }
-      // Push it into the data structure
+
+      // Push it into our data structure
       data.allItems[type].push(newItem);
 
       // Return the new element
@@ -67,7 +67,7 @@ var UIController = (function() {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputButton: ".add__btn",
+    inputBtn: ".add__btn",
     incomeContainer: ".income__list",
     expensesContainer: ".expenses__list"
   };
@@ -76,24 +76,27 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
     addListItem: function(obj, type) {
       var html, newHtml;
       // Create HTML string with placeholder text
+      console.log(obj);
 
       if (type === "inc") {
-        element = DOMstrings.incomeContainerl;
-        html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+        element = DOMstrings.incomeContainer;
+
+        html = `<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
-        html = `<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+
+        html = `<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
       }
 
       // Replace the placeholder text with actual data
-      newHtml = html.replace("%id%", opj.id);
+      newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
       newHtml = newHtml.replace("%value%", obj.value);
       // Insert the HTML into the DOM
@@ -111,9 +114,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   var eventListenerSetup = function() {
     var DOM = UICtrl.getDOMstrings();
 
-    document
-      .querySelector(DOM.inputButton)
-      .addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
       if (event.keyCode === 13 || event.which === 13) {
@@ -126,6 +127,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     var input, newItem;
     // 1. Get the input data
     input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add the item to budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
